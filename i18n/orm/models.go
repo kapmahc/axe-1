f65b48ac630ca2939bb3f2581edd1ee8338a4a62
Jeremy Zheng <jitang.zheng@gmail.com>
2017-08-25 18:39:12 -0700

    add json field to i18n model

--- i18n/orm/models.go
+++ i18n/orm/models.go
@@ -4,10 +4,10 @@ import "time"
 
 //Model locale model
 type Model struct {
-	ID        uint      `gorm:"primary_key"`
-	Lang      string    `gorm:"not null;type:varchar(8);index;unique_index:idx_locales_lang_code"`
-	Code      string    `gorm:"not null;index;type:VARCHAR(255);unique_index:idx_locales_lang_code"`
-	Message   string    `gorm:"not null;type:varchar(1024)"`
+	ID        uint      `gorm:"primary_key" json:"id"`
+	Lang      string    `gorm:"not null;type:varchar(8);index;unique_index:idx_locales_lang_code" json:"lang"`
+	Code      string    `gorm:"not null;index;type:VARCHAR(255);unique_index:idx_locales_lang_code" json:"code"`
+	Message   string    `gorm:"not null;type:varchar(1024)" json:"message"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
 }
